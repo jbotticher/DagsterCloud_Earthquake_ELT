29d2d192406738b68d43dd0c432cc14555428735
jbotticher <167915392+jbotticher@users.noreply.github.com>
2024-08-19 14:33:54 -0400

    Update dbt.py

--- dagster_elt/assets/dbt/dbt.py
+++ dagster_elt/assets/dbt/dbt.py
@@ -4,7 +4,7 @@ from dagster_dbt import DbtCliResource, dbt_assets
 from dagster import OpExecutionContext
 
 # configure dbt project resource
-dbt_project_dir = Path(__file__).joinpath("..", "..", "..", "dbt_earthquake", "warehouse").resolve()
+dbt_project_dir = Path(__file__).joinpath("..","..", "..", "..", "dbt_earthquake", "warehouse").resolve()
 dbt_warehouse_resource = DbtCliResource(project_dir=os.fspath(dbt_project_dir))
 
 
